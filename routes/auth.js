3975658b925059a489b90637f86440ed7c92fbbe
vinhnguyen2607 <vinh.nguyen2607@hcmut.edu.vn>
2023-12-12 21:48:26 +0700

    login

--- routes/auth.js
+++ routes/auth.js
@@ -33,19 +33,41 @@ const router = Router();
  *           $ref: '#/definitions/User'
  */
 router.post("/login", (req, res) => {
+  res.send("<b>/auth/login</b> works");
+
   try {
     
-    const userData = JSON.parse(fs.readFileSync("data/user.json", "utf8"));
+    const userData = JSON.parse(fs.readFileSync('./data/user.json', 'utf8'));
+    console.log(userData);
 
-    const { username, password } = req.body;
+    const _username = req.params['username'];
 
+    const _password = req.params['password'];
+
+    const list = userData['users'];
+    var item_username = null;
+    var item_password = null;
+  
+    for (let i = 0; i < list.length; i++){
+      if (list[i].username === _username){
+        item_username = list[i].username;
+        break;
+      }
+    }
+
+    for (let i = 0; i < list.length; i++){
+      if (list[i].password === _password){
+        item_username = list[i].password;
+        break;
+      }
+    }
     // Check if the provided username exists in the fetched data
-    if (userData.username !== username) {
+    if (item_password == null) {
       return res.status(404).json({ message: "Username not exist" });
     }
 
     // Check if the provided password matches the fetched password
-    if (userData.password !== password) {
+    if (userData.password == null) {
       return res.status(401).json({ message: "Wrong password" });
     }
 
